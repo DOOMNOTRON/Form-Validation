e6f79075bc24a68cf7cc002d18d00070a89d0ca8
edgar cruz <97191134+DOOMNOTRON@users.noreply.github.com>
2022-04-19 23:32:54 -0700

    Added function to test if tetxbox has data.

--- ts/main.ts
+++ ts/main.ts
@@ -4,5 +4,30 @@ window.onload = function(){
 }
 
 function main():void{
-    alert("Test");
+    isTextPresent("first-name", "First name is required.");
+    isTextPresent("last-name", "Last name is required.");
+}
+/**
+ * return true if the text box with the given id 
+ * has text inside of it.
+ * 
+ * @param id The id of the <input type="text" to validate>
+ * @param errMsg The message to display in the sibling spa of 
+ * the text box.
+ * @returns 
+ */
+ 
+ 
+function isTextPresent(id:string, errMsg:string):boolean {
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+
+    let txtBoxValue = txtBox.value;
+    if (txtBoxValue == "") {
+        let errSpan = 
+        <HTMLSpanElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+
+    return true;
 }
